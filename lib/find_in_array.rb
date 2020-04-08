2820cce35dc1a0ca748a6d55c7f2b3c65ad754be
Jessica K <jessikim807@gmail.com>
2020-04-08 15:54:25 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,9 +2,12 @@ def find_element_index(array, value_to_find)
   counter = 0
   found_value_index = nil
   while counter < array.length do
-    puts array[counter] == value_to_find
-    counter += 1
-
+    if array[counter] == value_to_find
+      found_value_index = counter
+      else
+        count += 1
+      end
+    end
   end
   found_value_index
 end
